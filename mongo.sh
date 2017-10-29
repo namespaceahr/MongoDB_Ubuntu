164be0ff39155ca9b4a81999be069c06d8f0be59
Rifat Ahmed Habibullah <ahrlab@live.com>
2017-10-30 01:43:31 +0600

    shell

--- mongo.sh
+++ mongo.sh
@@ -3,7 +3,7 @@
 read -r -p "Are you sure want to install MongoDB ? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
- sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
- 
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+fi
 OUTPUT="$(lsb_release -r)"
 echo "${OUTPUT}"
