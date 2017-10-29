9b651316a8df988ab2a3222a60340d265abed6a9
Rifat Ahmed Habibullah <ahrlab@live.com>
2017-10-30 01:46:17 +0600

    Delete mongo.sh

--- mongo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#scriptByNamespaceahr
-read -r -p "Are you sure want to install MongoDB ? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
-then
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-fi
-OUTPUT="$(lsb_release -r)"
-echo "${OUTPUT}"
